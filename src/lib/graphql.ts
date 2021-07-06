c2846c76891d0402c05a3978de2b023656575e27
Dexter <a.esselink@gmail.com>
2021-07-06 21:01:14 +0200

    Checking endpoint

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -2,6 +2,8 @@ import { writable } from 'svelte/store';
 import { gql, GraphQLClient } from 'graphql-request';
 import { GRAPHQL_ENDPOINT } from '$lib/config';
 
+console.log('Endpoint', GRAPHQL_ENDPOINT);
+
 export { gql };
 export const token = writable(null);
 export const client = new GraphQLClient(GRAPHQL_ENDPOINT);
