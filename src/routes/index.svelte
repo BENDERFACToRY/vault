8b14170b811ea3a9785173f5d06956ccee952194
Dexter <a.esselink@gmail.com>
2021-10-18 21:49:27 +0200

    Fixes login state caching bug

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,26 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import { page, session } from '$app/stores';
-	import { goto } from '$app/navigation';
-	import Media from '$lib/Media.svelte';
-
-	onMount(async () => {
-		if ($page.query.has('code') && $page.query.has('state')) {
-			// It's an oauth redirect call.
-			// Resolve the token and redirect the user to a clean url
-			const params = new URLSearchParams({
-				code: $page.query.get('code'),
-				state: $page.query.get('state')
-			});
-
-			const response = await fetch(`/auth/token?${params}`);
-			if (response.ok) {
-				const data = await response.json();
-				$session = {
-					...$session,
-					...data
-				};
-				goto('/');
-			}
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ session }) {
+		if (!session.user) {
+			return {
+				status: 302,
+				redirect: '/login'
+			};
 		}
-	});
+		return {
+			status: 200
+		};
+	}
 </script>
 
-{#if $session.user}
-	<Media />
-{:else}
-	<p>You need to login using <a href="auth/login">discord</a> in order to view any tracks</p>
-{/if}
+<script lang="ts">
+	import { session } from '$app/stores';
+	import Media from '$lib/Media.svelte';
+</script>
+
+<Media />
 
 <style>
 	:global(table tr.active) {
---
+++ src/routes/login.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { page, session } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import Media from '$lib/Media.svelte';
+
+	onMount(async () => {
+		if ($session.user) {
+			// User is logged in
+			goto('/');
+		} else if ($page.query.has('code') && $page.query.has('state')) {
+			// it's an oauth redirect call.
+			// Resolve the token and redirect the user to a clean url
+			const params = new URLSearchParams({
+				code: $page.query.get('code'),
+				state: $page.query.get('state')
+			});
+
+			const response = await fetch(`/auth/token?${params}`);
+			if (response.ok) {
+				const data = await response.json();
+				$session = {
+					...$session,
+					...data
+				};
+				goto('/');
+			}
+		}
+	});
+</script>
+
+{#if $page.query.has('code') && $page.query.has('state')}
+	<p>Please wait, we're logging you in.</p>
+{:else}
+	<p>You need to login using <a href="auth/login">discord</a> in order to view any tracks</p>
+{/if}
